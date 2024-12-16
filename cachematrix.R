a6f78115f4e897612674a1c12bc6402692360727
Renato <renato.vace@gmail.com>
2024-12-15 19:34:34 -0500

    Solución para el Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheTranspose <- function(x = matrix()) {
+  t_matrix <- NULL 
+  set <- function(y) {
+    x <<- y       
+    t_matrix <<- NULL  
+  }
+  get <- function() x  
+  settranspose <- function(transpose) t_matrix <<- transpose 
+  gettranspose <- function() t_matrix  
+  
+  list(set = set, get = get,
+       settranspose = settranspose,
+       gettranspose = gettranspose)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheTranspose <- function(x, ...) {
+  t_matrix <- x$gettranspose()
+  if (!is.null(t_matrix)) {
+    message("getting cached transpose")
+    return(t_matrix)
+  }
+  data <- x$get()  
+  t_matrix <- t(data)  
+  x$settranspose(t_matrix) 
+  t_matrix  
 }
+
